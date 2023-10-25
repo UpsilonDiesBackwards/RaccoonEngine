f38c6d4f73b0bd61b3bb6d6dd89ba305448fb151
Tayler Parsons <upsilon@diesbackwards.com>
2023-10-25 02:22:02 +0100

    remove redundant comment

--- src/engine/renderer/ModelRenderer.cpp
+++ src/engine/renderer/ModelRenderer.cpp
@@ -12,7 +12,7 @@ void ModelRenderer::Initialize() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // Need to change this! (probably)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
